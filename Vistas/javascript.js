ef9aa5de3d7cd6f93afba9211f6fc6ff3a0ba2a7
YJoel <91228862+YJoel@users.noreply.github.com>
2022-09-06 16:48:19 -0500

    Add files via upload

---
+++ Vistas/javascript.js
@@ -0,0 +1,20 @@
+function generar(){
+    let nombre = "EL MANDA CALLA";
+    let nombres = document.querySelectorAll(".nombre");
+    
+    let desc = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim corporis saepe porro? Eos impedit, possimus repudiandae ipsa repellendus autem! Perspiciatis cupiditate blanditiis deleniti sed rerum alias saepe a fugiat possimus.";
+    let descripcion = document.querySelectorAll(".descripcion");
+    
+    for (let i = 0; i < nombres.length; i++) {
+        nombres[i].innerHTML = nombre;
+    }
+    for (let i = 0; i < descripcion.length; i++) {
+        descripcion[i].innerHTML = desc;
+    }
+}
+
+function subir() {
+    var data = document.createElement("INPUT");
+    data.setAttribute("type", "file");
+    document.body.appendChild(data);
+}
